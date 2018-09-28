055d833d00fdf989cff8503b10b7849cf0f53202
Johanna De Bruycker <johanna.debruycker@student.hogent.be>
2018-09-28 12:21:40 +0200

    Create class BankAccount

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models.Domain
+{
+    public class BankAccount
+    {
+        #region Fields
+        //Fields ipv attributen
+
+        //private string _accountNumber;
+        private decimal _balance;
+
+        //constante definiëren = sowieso static!
+        //geldwaarden altijd met decimal
+        //met M erachter, double bekijken als decimal
+        //D voor double
+        //  public const decimal WithdrawCost = 0.10M;
+
+        //readonly, kan waarde meegegeven worden hier, of via constructor ingegeven worden
+        //niet static, moet niet, als je hem static maakt kan je hem niet instatiëren in de constructor
+        //  public static readonly decimal _withdrawCost = 0.10M;
+
+        //public decimal getbalance()
+        //{
+        //    return _balance;
+        //}
+
+        //private void setbalance(decimal value)
+        //{
+        //    _balance = value;
+        //} 
+        #endregion
+
+        #region Properties
+        //property AccountNumber, standaard getter en setter op deze manier
+        //prop & 2 keer tab, dan krijg je skelet van property
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Balance cannot be negative");
+                //value is een keyword, dat enkel bij setter kan gebruikt worden
+                _balance = value;
+            }
+        }
+
+        public string AccountNumber { get; private set; } 
+        #endregion
+
+        #region Constructors
+        //ctor, 2 keer tab = constructor
+        public BankAccount(string accountNumber) : this(accountNumber, 0)
+        {
+        }
+
+        //constructor aanroepen via :this(parameter)
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            //_withdrawCost = 0.10M;
+        }
+        #endregion
+
+        #region Methods
+        //int nrOfTimes = optionele parameter, moet achteraan de parameterlijst staan, met defaultwaarde
+        public void Deposit(decimal amount/*, int nrOfTimes = 1*/)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Amount cannot be negative");
+            Balance += amount/* * nrOfTimes*/;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Amount cannot be negative");
+            Balance -= amount;
+        } 
+        #endregion
+    }
+}
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,23 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myBA = new BankAccount("123-12312312-99");
+            Console.WriteLine($"Account {myBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            //Console.WriteLine($"Withdrawcost is {BankAccount.WithdrawCost} Euro");
+
+
+            BankAccount myOtherBA = new BankAccount("123-32112332-99", 50);
+            Console.WriteLine($"Account {myOtherBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myOtherBA.Balance} Euro");
+
+            myBA.Deposit(1000);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Deposit(200/*, 3*/);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(100);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.ReadKey();
         }
     }
 }
